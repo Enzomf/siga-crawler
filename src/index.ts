0c5e779f3a9645b67f0fa3e092da370f155bff41
enzomf <enzomfigueiredo11@gmail.com>
2024-10-21 21:34:35 -0300

    feat: remove função de teste

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import { credentials } from "../test_credentials.json";
-import { Siga } from "./entities/siga";
 
 export * from "./entities/aluno";
 export * from "./entities/siga";
 
-const siga = new Siga();
-
-async function main() {
-  for (const credential of credentials) {
-   const aluno = await siga.getInformacoesAluno(credential);
-    if(!aluno) console.log('success true: ', false);
-}
-  main();
-}
-main();
